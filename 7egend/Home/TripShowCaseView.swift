4a46553891bff7905bec9d8ac296b760a96d4711
Leonel Menezes <mml_leo@hotmail.com>
2021-04-01 02:32:17 +0100

    Fix layout

--- 7egend/Home/TripShowCaseView.swift
+++ 7egend/Home/TripShowCaseView.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 final class TripShowCaseView: UIView {
+    let overlay = UIView().makeCodableLayoutView()
     let imageView = UIImageView().makeCodableLayoutView()
     let seasonIcon = UIImageView().makeCodableLayoutView()
     let nameLabel = UILabel().makeCodableLayoutView()
@@ -25,29 +26,37 @@ final class TripShowCaseView: UIView {
 
 extension TripShowCaseView: CodableViewLayout {
     func buildViewHierarchy() {
-        [imageView, seasonIcon, nameLabel, periodLabel].forEach(addSubview)
+        [imageView, overlay, seasonIcon, nameLabel, periodLabel].forEach(addSubview)
     }
     
     func constraintViews() {
+        overlay.constrainToEdges()
         imageView.constrainToEdges()
         periodLabel.constrainLeading(spacing: 32)
         periodLabel.constrainBottom(spacing: 32)
         nameLabel.constrainBottom(to: periodLabel, spacing: 16)
         nameLabel.constrainLeading(to: periodLabel)
-        seasonIcon.constrainBottom(to: nameLabel, spacing: 16)
+        seasonIcon.constrainTop(to: nameLabel, spacing: -32)
         seasonIcon.constrainLeading(to: nameLabel)
-        seasonIcon.constrainSize(height: 16, width: 16)
+        seasonIcon.constrainSize(height: 32, width: 32)
     }
     
     func additionalSetup() {
         setupLayer()
+        setupOverlay()
         setupNameLabelAppearance()
         setupPeriodLabelAppearance()
+        setupSeasonIcon()
     }
     
     private func setupLayer() {
         clipsToBounds = true
         layer.cornerRadius = 16
+    }
+    
+    private func setupOverlay() {
+        overlay.backgroundColor = .black
+        overlay.alpha = 0.3
     }
     
     private func setupNameLabelAppearance() {
@@ -57,14 +66,18 @@ extension TripShowCaseView: CodableViewLayout {
     
     private func setupPeriodLabelAppearance() {
         periodLabel.textColor = .white
-        periodLabel.font = UIFont.systemFont(ofSize: 8, weight: .medium)
+        periodLabel.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+    }
+    
+    private func setupSeasonIcon() {
+        seasonIcon.tintColor = .white
     }
 }
 
 extension TripShowCaseView: ConfigurableView {
     func setup(with viewModel: TripShowCaseViewModel) {
         imageView.image = UIImage(named: viewModel.imageSource)
-        seasonIcon.image = UIImage(named: viewModel.seasonImageSource)
+        seasonIcon.image = UIImage(systemName: "leaf")
         nameLabel.text = viewModel.name
         periodLabel.text = viewModel.period
     }
